a82720c1026835203cd092a4aba5223302daf673
Rajender <rajenderkatkuri123@gmail.com>
2023-04-13 21:33:18 +0530

    Add recoil and implement showform atom

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import Links from "./components/Links";
 import NewLink from "./components/NewLink";
+import { useRecoilValue } from "recoil";
+import { showFormAtom } from "./recoil/atom/showFormAtom";
 
 export default function Popup(): JSX.Element {
-  const [showForm, setShowForm] = useState(false);
+  const showForm = useRecoilValue(showFormAtom);
 
   return (
     <div className="max-h-[200px]">
---
+++ src/pages/popup/recoil/atom/showFormAtom.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const showFormAtom = atom({
+  key: "showForm",
+  default: false,
+});
